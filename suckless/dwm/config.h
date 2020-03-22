905aeb98b66aa9f0d821c938b1ff7cde393bfe51
TecPr0gr4mm3r <RBQIEO@protonmail.com>
2020-03-21 22:31:31 -0300

    changes

--- suckless/dwm/config.h
+++ suckless/dwm/config.h
@@ -6,11 +6,9 @@ static const unsigned int gappx     = 5;        /* gap pixel between windows */
 static const unsigned int snap      = 5;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 0;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=10",
-										"Font Awesome 5 Free Regular-8",
-										"Font Awesome 5 Free Solid-8"
-										"Font Awesome 5 Free Brand-8"
-
+static const char *fonts[] = {
+    "FontAwesome:size=10",
+	"monospace:size=10",
 };
 static const char col_gray1[]       = "#000000"; /* Non-activated bar background */
 static const char col_gray2[]       = "#000000"; /* Non-activated window border */
@@ -23,7 +21,7 @@ static const char *colors[][3]      = {
 	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
 };
 /* tagging */
-static const char *tags[] = { "", "", "", "", "", "","7", "8", "9" };
+static const char *tags[] = { "1", "2", "3", "4", "5", "6","7", "8", "9" };
 
 static const Rule rules[] = {
 	/* class      instance    title       tags mask     iscentered     isfloating   monitor */
